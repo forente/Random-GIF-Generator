890e5765ce1b85790e3fa2391bc5505c8d1d7da7
forente <zerofox16@gmail.com>
2016-07-02 18:14:40 -0500

    Added random Button functionality

--- gifs.js
+++ gifs.js
@@ -2,10 +2,21 @@ $(document).ready(function(){
 
   $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
   .then(function(ob){
-    $('body').append('<img src='+ ob.data.image_url +'>');
-    console.log(ob.data);
+    $('#gifBucket').append('<img src='+ ob.data.image_url +'>');
   })
-  .catch;
+  .catch(function(){
+    console.log('Something went wrong!');
+  });
+
+  $('#random').on('click',function(){
+    $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
+    .then(function(ob){
+      $('#gifBucket').html('<img src='+ ob.data.image_url +'>');      
+    })
+    .catch(function(){
+      console.log('Something went wrong!');
+    });
+  });
 
 
 });

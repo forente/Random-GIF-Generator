8667a6b18177ee34fbd5d30cd45d2bdafa32e58e
forente <zerofox16@gmail.com>
2016-07-03 00:51:46 -0500

    Made function get Gif

--- gifs.js
+++ gifs.js
@@ -1,27 +1,24 @@
 $(document).ready(function(){
 
-  $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC')
-  .then(function(ob){
-    $('#gifBucket').append('<img src='+ ob.data.image_url +'>');
-  })
-  .catch(function(){
-    console.log('Something went wrong!');
+    getGif('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC');
+// load in the
+  $('#random').on('click',function(){
+    //
+    var theTag = $('#tag').val();
+    theTag = theTag.replace(" ","+");
+
+    getGif('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+theTag);
   });
 
-  $('#random').on('click',function(){
-
-    var theTag = $('#tag').val();
-    theTag = theTag.replace(" ","+")
-    
-
-    $.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+theTag)
+  function getGif ( link){
+    $.get(link)
     .then(function(ob){
       $('#gifBucket').html('<img src='+ ob.data.image_url +'>');
     })
     .catch(function(){
       console.log('Something went wrong!');
-    });
-  });
+    })
+  }
 
 
 });

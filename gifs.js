1344fdfe7d0ab2787b3140b0ae81f3d8325f1611
forente <zerofox16@gmail.com>
2016-07-05 09:00:01 -0500

    Added Sricker button

--- gifs.js
+++ gifs.js
@@ -1,16 +1,27 @@
 $(document).ready(function(){
 
     getGif('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC');
-// load in the
-  $('#random').on('click',function(){
+// load in the tag and remove the spaces for the address bar
+  $('#randomGif').on('click',function(){
     //
     var theTag = $('#tag').val();
     theTag = theTag.replace(" ","+");
 
     getGif('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag='+theTag);
   });
+  $('#randomStick').on('click',function(){
+    //
+    var theTag = $('#tag').val();
+    theTag = theTag.replace(" ","+");
 
-  function getGif ( link){
+    getSticker('http://api.giphy.com/v1/stickers/random?api_key=dc6zaTOxFJmzC&tag='+theTag);
+
+  });
+
+
+//
+//functions
+  function getGif (link){
     $.get(link)
     .then(function(ob){
       $('#gifBucket').html('<img src='+ ob.data.image_url +'>');
@@ -19,6 +30,23 @@ $(document).ready(function(){
       console.log('Something went wrong!');
     })
   }
+
+  function getSticker (link){
+    $.get(link)
+    .then(function(ob){
+      $('#gifBucket').html('<img src='+ ob.data.image_original_url +'>');
+
+    })
+    .catch(function(){
+      console.log('Something went wrong!');
+    })
+  }
+
+
+
+
+
+
 
 
 });
